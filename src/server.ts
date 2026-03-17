3b9ffb97d4b552de4ec8cb449d8d8a51b7d217d8
MD Sultan Mia <sultanmia5732@gmail.com>
2026-03-18 03:12:11 +0600

    added express server with mongose

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import config from "./config";
+import app from "./app";
+
+async function main(){
+    try{
+        if(!config.database_url){
+            throw new Error('Database URL is not provided in environment variables')
+        }
+
+        await mongoose.connect(config.database_url);
+        console.log('Connect to MongoDB Successfully');
+
+        app.listen(config.port,() => {
+            console.log(`Server is listening on port ${config.port}`);
+        });
+    }
+    catch(er){
+    console.error('Failed to connect to MongoDB', er);
+    process.exit(1);
+    }
+}
+
+main()
