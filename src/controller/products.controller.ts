68b3c9c8468b5213e42a366c889ee7a80c80fe63
MD Sultan Mia <sultanmia5732@gmail.com>
2026-03-28 13:26:07 +0600

    added all product api route

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -21,6 +21,75 @@ const popularProducts = async (req: Request, res: Response) => {
     }
 }
 
+const getFilters = async (req: Request, res: Response) => {
+    try {
+        const categories = await Products.distinct('category');
+        const brands = await Products.distinct('brand');
+
+        res.status(200).json({
+            success: true,
+            data: { categories, brands }
+        });
+    }
+    catch (er: any) {
+        res.status(500).json({
+            success: false,
+            message: 'Something went Wrong!'
+        })
+    }
+}
+
+const allProduct = async (req: Request, res: Response) => {
+    try {
+        const { type, value } = req.query;
+
+        let query: any = {};
+        let sort: any = {};
+
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 8;
+        const skip = (page - 1) * limit;
+
+
+        if ((type === 'category' || type === 'brand') && value !== 'all') {
+            query[type] = value;
+        }
+
+        if (type === 'search' && typeof value === 'string' && value.trim() !== '') {
+            query['name'] = { $regex: value, $options: 'i' };
+        }
+
+        if (type === 'sort') {
+            if (value === 'low') sort.price = 1;
+            if (value === 'high') sort.price = -1;
+        }
+
+        const product = await Products.find(query)
+            .sort(sort)
+            .skip(skip)
+            .limit(limit);
+
+
+        res.status(200).json({
+            success: true,
+            message: "Filtered products fetched successfully",
+            data: product,
+        });
+
+    }
+    catch (er: any) {
+        console.error(er);
+
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+        });
+    }
+}
+
+
 export const productController = {
-    popularProducts
+    popularProducts,
+    getFilters,
+    allProduct
 }
--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -4,5 +4,7 @@ import { productController } from '../controller/products.controller';
 const router = express.Router();
 
 router.get('/popularProduct',productController.popularProducts)
+router.get('/filters',productController.getFilters)
+router.get('/allProduct',productController.allProduct)
 
 export const productRoute = router
