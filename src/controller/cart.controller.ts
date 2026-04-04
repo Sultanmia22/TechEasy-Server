0183ef42cc3bfc1e4ae1084eabc1e32372995e62
MD Sultan Mia <sultanmia5732@gmail.com>
2026-04-05 01:52:48 +0600

    add: delete cart api route

--- src/controller/cart.controller.ts
+++ src/controller/cart.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Cart from "../models/cart.model";
 import type { AuthRequest } from "../middleware/authMiddleware";
+import { Types } from "mongoose";
 
 const addToCart = async (req: Request, res: Response) => {
   try {
@@ -102,7 +103,48 @@ const getCartByEmail = async (req: AuthRequest, res: Response) => {
   }
 };
 
+const removeCart = async (req: Request, res: Response) => {
+  try{
+    const {id} = req.params as { id: string }
+
+   if (!Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid ID format",
+      });
+    }
+
+    const objId = new Types.ObjectId(id);
+
+    const result = await Cart.updateOne(
+      {"items.productId":objId},
+      {
+        $pull: {items: {productId:objId}}
+      }
+    )
+
+    if (result.modifiedCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found in cart",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Product successfully remove from cart',
+    })
+  }
+
+  catch(er:any){
+
+    console.log(er)
+  }
+
+}
+
 export const cartController = {
   addToCart,
-  getCartByEmail
+  getCartByEmail,
+  removeCart
 };
