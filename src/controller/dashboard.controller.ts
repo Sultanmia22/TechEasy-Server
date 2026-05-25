5448f73768b79cdc4a739d9bf82ae23993587027
MD Sultan Mia <sultanmia5732@gmail.com>
2026-05-25 20:39:47 +0600

    feat: implement dashboard analytics and stats aggregation
    - Added  aggregation for unified stats, recent orders, top products, and recent users
    - Implemented role-based dashboard data fetching
    - Optimized API response with Promise.all and lookup logic

--- src/controller/dashboard.controller.ts
+++ src/controller/dashboard.controller.ts
@@ -2,12 +2,16 @@ import type { Request, Response } from "express";
 import { CustomerOrder } from "../models/order.mode";
 import { WishList } from "../models/wishlist.model";
 import type { AuthRequest } from "../middleware/authMiddleware";
+import { User } from "../models/user.model";
 
 const getDashboradSummyData = async (req: AuthRequest, res: Response) => {
     try {
-        const { customerEmail } = req.query;
 
-        if (!customerEmail) {
+        const {user,role} = req.user
+
+        const { email } = req.query;
+
+        if (!email) {
             res.status(400).json({
                 success: false,
                 message: 'Customer Email is required. Please provide a valid email.'
@@ -15,12 +19,94 @@ const getDashboradSummyData = async (req: AuthRequest, res: Response) => {
             return;
         }
 
+
+        if(role === 'admin') {
+            const orderData = await CustomerOrder.aggregate([
+                { $unwind: "$orders" },
+                {
+                  $facet: {
+                    "stats": [
+                      {
+                        $group: {
+                          _id: null,
+                          totalOrders: { $sum: 1 },
+                          totalSales: { $sum: "$orders.totalPrice" },
+                          totalPending: { $sum: { $cond: [{ $eq: [{ $arrayElemAt: ["$orders.orderStatus", -1] }, "pending"] }, 1, 0] } },
+                          totalDelivered: { $sum: { $cond: [{ $eq: [{ $arrayElemAt: ["$orders.orderStatus", -1] }, "delivered"] }, 1, 0] } }
+                        }
+                      }
+                    ],
+                    "recentOrders": [
+                      { $sort: { "orders.createdAt": -1 } },
+                      { $limit: 5 },
+                      {
+                        $lookup: {
+                          from: "users",
+                          localField: "email",
+                          foreignField: "email",
+                          as: "customerInfo"
+                        }
+                      },
+                      {
+                        $project: {
+                          productImage: { $arrayElemAt: ["$orders.items.image", 0] }, // Item er image
+                          customerName: { $arrayElemAt: ["$customerInfo.name", 0] },
+                          productName: { $arrayElemAt: ["$orders.items.name", 0] },
+                          amount: "$orders.totalPrice",
+                          status: { $arrayElemAt: ["$orders.orderStatus", -1] }
+                        }
+                      }
+                    ],
+                    "topProducts": [
+                      { $unwind: "$orders.items" },
+                      {
+                        $group: {
+                          _id: "$orders.items.productId",
+                          productName: { $first: "$orders.items.name" },
+                          productImage: { $first: "$orders.items.image" },
+                          totalQty: { $sum: "$orders.items.quantity" },
+                          totalAmount: { $sum: { $multiply: ["$orders.items.price", "$orders.items.quantity"] } }
+                        }
+                      },
+                      { $sort: { totalQty: -1 } },
+                      { $limit: 5 }
+                    ]
+                  }
+                }
+              ]);
+          
+              // 2. Recent Users (User collection theke)
+              const recentUsers = await User.find({})
+                .sort({ createdAt: -1 })
+                .limit(5)
+                .select("name image email createdAt");
+          
+              // Result structure
+              const data = orderData[0];
+              const stats = data.stats[0] || { totalOrders: 0, totalSales: 0, totalPending: 0, totalDelivered: 0 };
+              
+              // console.log(data)
+              return res.status(200).json({
+                data: {
+                  stats: {
+                    totalOrder: stats.totalOrders,
+                    totalRevenue: stats.totalSales,
+                    totalPendingOrder: stats.totalPending,
+                    totalDeliveredOrder: stats.totalDelivered
+                  },
+                  recentOrders: data.recentOrders,
+                  topProducts: data.topProducts,
+                  recentUsers: recentUsers
+                }
+              });
+        }
+
         const oneHourAgo = new Date(Date.now() - 5 * 60 * 60 * 1000);
 
         const [orderStats, wishlistData, recentOrdersData] = await Promise.all([
 
             CustomerOrder.aggregate([
-                { $match: { email: customerEmail } },
+                { $match: { email: email } },
                 {
                     $project: {
                         totalOrder: { $size: "$orders" },
@@ -47,7 +133,7 @@ const getDashboradSummyData = async (req: AuthRequest, res: Response) => {
             ]),
 
 
-            WishList.findOne({ customerEmail })
+            WishList.findOne({ customerEmail: email })
                 .populate({
                     path: 'wishListItem.productId'
                 })
@@ -55,7 +141,7 @@ const getDashboradSummyData = async (req: AuthRequest, res: Response) => {
 
 
             CustomerOrder.findOne({
-                email: customerEmail,
+                email: email,
                 "orders.orderDate": { $gte: oneHourAgo }
             })
                 .populate({
