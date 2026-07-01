ac7bdf0d01fb6725efc70dac5c0470fff13087c6
MD Sultan Mia <sultanmia5732@gmail.com>
2026-07-01 16:20:17 +0600

    feat: update profile controller

--- src/controller/profile.controller.ts
+++ src/controller/profile.controller.ts
@@ -2,9 +2,19 @@ import type { Request, Response } from "express"
 import { User } from "../models/user.model"
 import { Products } from "../models/products.model"
 import { CustomerOrder } from "../models/order.mode"
+import type { AuthRequest } from "../middleware/authMiddleware"
 
-const adminStats = async (req: Request, res: Response) => {
+const adminStats = async (req: AuthRequest, res: Response) => {
     try {
+
+        const {role} = req.user
+
+        if(role !== 'admin'){
+            return res.status(401).json({
+                success : false,
+                message : 'You are unauthirized for this action'
+            })
+        }
 
         const [totalUsers, totalProducts, totalOrders, revenueData] = await Promise.all([
             User.countDocuments(),
